388e60a3ce2ae3a78b6cafb5fa30a176cef6b040
Vasan j k <114376438+Vasan-jk@users.noreply.github.com>
2025-06-15 22:01:04 +0530

    Update counter5x2.v

--- counter5x2.v
+++ counter5x2.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:    19:20:12 08/28/24
-// Design Name:    
-// Module Name:    app5_2
-// Project Name:   
-// Target Device:  
-// Tool versions:  
-// Description:
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-///////////////////////////////////////////////////////////////////////////////
-
 module app5_2(s,ca,a,b,c,d,e);
 input a,b,c,d,e;
 output s,ca;
